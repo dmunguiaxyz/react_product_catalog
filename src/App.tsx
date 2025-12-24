f4a9db232b72ff699a543a1599aa3582ec7257c4
damunguiae <damunguia@proton.me>
2025-12-24 13:23:44 -0600

    Add NavigationBar component and integrate it into the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ProductList from "./Components/ProductList/ProductList";
-import { useState } from "react";
 import SearchBar from "./Components/SearchBar/searchBar";
+import NavigationBar from "./Components/NavigationBar/navigationBar";
+import { useState } from "react";
 
 export default function App(){
     const [searchTerm, setSearchTerm] = useState("")
@@ -8,7 +9,7 @@ export default function App(){
         //this will trigger a re-render on each input change
         setSearchTerm(event.target.value);
     }
-    return (<> <h1>Welcome to the Product Catalog App</h1>
+    return (<> <div><NavigationBar></NavigationBar></div>
     <div>
         <SearchBar handleInputChange={handleInputChange}></SearchBar>
     </div>
